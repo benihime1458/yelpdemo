b580433a14d087d3dfa67316a6c1059d5704ce72
benihime1458 <benihime1458@gmail.com>
2015-09-22 11:24:56 -0700

    linked Reviews to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922012130) do
+ActiveRecord::Schema.define(version: 20150922181205) do
 
   create_table "reivews", force: :cascade do |t|
     t.integer  "rating"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150922012130) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
