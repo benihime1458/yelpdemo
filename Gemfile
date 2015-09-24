46ff9fe2e55bf527440ca44d149ffdd8abeda25d
benihime1458 <benihime1458@gmail.com>
2015-09-24 13:43:59 -0700

    added search bar

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem "fog-aws"
 gem "figaro"
 gem 'mini_magick'
 gem 'devise'
+gem 'searchkick'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,11 @@
 class Restaurant < ActiveRecord::Base
   mount_uploader :image, ImageUploader
-  has_many :reviews
-  validates :name, :address, :phone, :website, :image, presence: true
 
+  searchkick
+
+  has_many :reviews
+
+  validates :name, :address, :phone, :website, :image, presence: true
   validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
   message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
   validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
