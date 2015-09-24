2683ec10e8f721062819ddd8b671c0a04c0eccfa
benihime1458 <benihime1458@gmail.com>
2015-09-24 11:11:28 -0700

    added names into restaurant show page

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150924171135) do
 
-  create_table "reivews", force: :cascade do |t|
-    t.integer  "rating"
-    t.text     "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
