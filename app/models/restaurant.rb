c7b62428643161099d3188cdc3c6fd56f1170542
benihime1458 <benihime1458@gmail.com>
2015-09-24 11:40:46 -0700

    added validations for review/restaurant forms

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,12 @@
 class Restaurant < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   has_many :reviews
+  validates :name, :address, :phone, :website, :image, presence: true
+
+  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+  message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+  message: "must be in the format (123) 456-7890" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+  message: "must start with http:// or https://" }
 end
